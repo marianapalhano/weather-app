d17947d466c35fe83226927a430705bb76a8e7e4
Mariana <marianaufc@gmail.com>
2021-08-25 11:30:51 -0300

    Included API parameters

---
+++ scripts/accuweather.js
@@ -0,0 +1,5 @@
+
+const KEY = 'xwOeLNda1DJnlbWyyH4jAFYGSVGAcD1l';
+const weatherSearch = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/';
+const citySearch = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+const currentSearch = 'http://dataservice.accuweather.com/currentconditions/v1/';
