117bcce2a44e79004472caa30d2e53338f7f1a75
Mariana <marianaufc@gmail.com>
2021-08-26 19:21:11 -0300

    Axios configured, getting city key

--- scripts/accuweather.js
+++ scripts/accuweather.js
@@ -3,3 +3,19 @@ const KEY = 'xwOeLNda1DJnlbWyyH4jAFYGSVGAcD1l';
 const weatherSearch = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/';
 const citySearch = 'http://dataservice.accuweather.com/locations/v1/cities/search';
 const currentSearch = 'http://dataservice.accuweather.com/currentconditions/v1/';
+
+// const axios = require('axios');
+
+const getCity = async () => {
+    const response = await axios.get('http://dataservice.accuweather.com/locations/v1/cities/search', {
+        params: {
+            apikey: KEY,
+            q: 'fortaleza'
+        }
+    })
+    const cityKey = await response.data[0].Key;
+    return cityKey;
+}
+
+getCity()
+    .then( data => console.log('resolved', data))
