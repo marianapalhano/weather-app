130abb4b612c56439d4b43d5642f5c48f0a4e7f7
Mariana <marianaufc@gmail.com>
2021-09-01 11:58:25 -0300

    Weather details result showing on screen

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,58 @@
-//import './accuweather';
-
 const searchForm = document.querySelector('.weather-search');
+const details = document.querySelector('.weather-details');
 
 const updateCity = async (city) => {
     const cityKey = await getCityKey(city);
     const weather = await getWeather(cityKey);
     
-    return weather[0];
+    return { city: city, weather: weather };
 }
 
+const updateUI = (data) => {
+    console.log(data);
+    //switch para imagem svg
+    //cloud-rain
+    //cloud
+    //wind
+    //sun
+    //cloud-lightning
+    let icon;
+
+    switch(data.weather[0].WeatherText) {
+        case "Sunny":
+            icon = "sun";
+            break;
+        case "Partly sunny":
+            icon = "sun";
+            break;
+        case "Cloudy":
+            icon = "cloud";
+            break;
+        case "Showers":
+            icon = "cloud-rain";
+            break;
+        case "Rain":
+            icon = "cloud-rain";
+            break;
+        case "Windy":
+            icon = "wind";
+            break;
+        default:
+            icon = "cloud";
+    }
+
+    details.innerHTML = `
+        <i data-feather=${icon}></i>
+        <h5>${data.city}</h5>
+        <div class="weather-condition">
+            <div>
+                <span>${data.weather[0].WeatherText}</span>
+                <span>${data.weather[0].Temperature.Metric.Value}</span><span>&deg;C</span>
+            </div>
+        </div>
+    `;
+    feather.replace();
+}
 
 searchForm.addEventListener('submit', evt => {
     evt.preventDefault();
@@ -16,6 +60,7 @@ searchForm.addEventListener('submit', evt => {
     searchForm.reset();
 
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err));
 })
+
